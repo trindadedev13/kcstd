7ea3d47de569b080bee9a9b868af5c1d8eef9357
trindadedev13 <trindadedev13@gmail.com>
2025-07-08 21:04:10 -0300

    feat(examples): Add example

---
+++ example/build.rb
@@ -0,0 +1,35 @@
+require 'fileutils'
+
+def run(cmd)
+  puts cmd
+  system(cmd) or abort("Failed #{cmd}")
+end
+
+FileUtils.mkdir_p("build")
+
+files = [
+  "main.c"
+].join(" ")
+
+args = [
+  "#{files}",
+  "-nostdlib",
+  "-nostartfiles",
+  "-nodefaultlibs",
+  "-fno-builtin",
+  "-ffreestanding",
+  "-Iinclude/",
+  "-lkcstd",
+  "-o build/main",
+].join(" ")
+
+run("gcc #{args}")
+
+HOME = ENV["HOME"]
+FileUtils.mkdir_p("#{HOME}/temp/kcstd_example/")
+FileUtils.cp("build/main", "#{HOME}/temp/kcstd_example/main")
+
+Dir.chdir("#{HOME}/temp/kcstd_example") do
+  run("chmod +x main")
+  run("./main")
+end
---
+++ example/main.c
@@ -0,0 +1,77 @@
+#include <kcstd/io.h>
+#include <kcstd/memory.h>
+#include <kcstd/types.h>
+
+struct people {
+  string name;
+};
+
+int main() {
+  struct people* me = memory_alloc(sizeof(struct people));
+  if (me == null) {
+    printf("Failed to allocate memory for 'struct people me'\n");
+    return 1;
+  }
+  me->name = "Aquiles Trindade";
+
+  printf("My name is %s\n", me->name);
+
+  memory_free(me);
+
+  // str_len(src)
+  printf("ola size: %d\n", str_len("Ola"));
+
+  // str_copy_lmt(src, dest, lmt)
+  string cc = "gcc";
+  size_t tt = str_len(cc) + 1;
+  string cp = memory_alloc(tt);
+  str_copy_lmt(cp, cc, 2);
+
+  printf("%s\n", cp);
+
+  // str_cat(src, new)
+  string text_a = "Hello, ";
+  string text_b = "World!";
+  string text = memory_alloc(str_len(text_a) + str_len(text_b));
+  str_cat(text, text_a);
+  str_cat(text, text_b);
+  printf("%s\n", text);
+
+  // str_equals(src, compare)
+  bool ba = str_equals(text_a, "Hello, ");
+  printf("text_a Equals \"Hello, \" ? : %s\n", ba ? "true" : "false");
+
+  bool bb = str_equals(text_b, "Hello, ");
+  printf("text_b Equals \"Hello, \" ? : %s\n", bb ? "true" : "false");
+
+  memory_free(text);
+
+  // Write hi in main.txt
+  file* txt = file_open("main.txt", FILE_MODE_WRITE);
+  if (txt == null) {
+    printf("Failed to open main.txt.\n");
+    return 1;
+  }
+  write(txt, "hi", str_len("hi"));
+  file_close(txt);
+
+  // read 128 bytes in main.txt
+  file* rtxt = file_open("main.txt", FILE_MODE_READ);
+  if (rtxt == null) {
+    printf("Failed to open main.txt.\n");
+    return 1;
+  }
+  string dest = memory_alloc(128);
+  read(rtxt, dest, 128);
+  printf("File content: %s\n");
+
+  return 0;
+}
+
+void _start() {
+  // used for inicialize pool memory
+  // im not using it now.
+  // memoryp_init();
+  int result = main();
+  exit(result);
+}
