01d470c819fbba74c43ab73347ae408f4412ab32
trindadedev13 <trindadedev13@gmail.com>
2025-07-08 21:03:12 -0300

    feat(Buildscript): Add cache in build

--- build.rb
+++ build.rb
@@ -6,21 +6,22 @@ def run(cmd)
 end
 
 def help()
-  puts "Klt-CStd Buildscript"
-  puts "--run  or -r   | Run after build."
-  puts "--help or -h   | Shows helps."
+  puts "KCStd Buildscript"
+  puts "--install or -i   | Installs after build."
+  puts "--clean or -c     | Clean files before build."
+  puts "--help or -h      | Shows helps."
   exit 1
 end
 
-def install_kstd()
+def install()
   prefix = ENV["PREFIX"]
   FileUtils.cp("build/libkcstd.a", "#{prefix}/lib/libkcstd.a")
-  puts "Installing #{prefix}/lib/libkcstd.a"
+  puts "-- Installing #{prefix}/lib/libkcstd.a"
   FileUtils.mkdir_p("#{prefix}/include/kcstd")
   Dir.glob("include/kcstd/*").each do |header|
     header_filename = header.split("/").last
     out_dir = "#{prefix}/include/kcstd/#{header_filename}"
-    puts "Installing #{out_dir}"
+    puts "-- Installing #{out_dir}"
     FileUtils.cp_r(header, out_dir)
   end
 end
@@ -38,69 +39,91 @@ if arch == :unknown
   exit(1)
 end
 
-puts "Building in #{arch}"
+puts "-- Building in #{arch}"
 
 asmbuild_args = [
   "-nostdlib",
   "-nodefaultlibs",
   "-c -fPIC"
-]
+].join(" ")
 
 cbuild_args = [
   "-std=c99",
   "-nostdlib",
-  "-nostartfiles",
   "-nodefaultlibs",
   "-fno-builtin",
   "-ffreestanding",
   "-static",
   "-Iinclude/"
-]
+].join(" ")
 
-run = false
-install = false
+install_arg = false
 
 if ARGV.length >= 1
   ARGV.each do |arg|
     case arg
       when "-h", "--help"
         help()
-      when "-r", "--run"
-        run = true
       when "-i", "--install"
-        install = true
+        install_arg = true
+      when "-c", "--clean"
+        FileUtils.remove_dir("build")
     end
   end
 end
 
 FileUtils.mkdir_p("build")
 
-asm_files = Dir.glob("src/asm/#{arch}/*.s")
-asm_files.each do |asm_file|
-  out_filename = asm_file.split("/").last.sub(".s", ".o")
-  asmbuild_args_str = asmbuild_args.join(" ")
-  run("gcc #{asmbuild_args_str} #{asm_file} -o build/#{out_filename}")
-end
+# Compile All Assembly Sources
+Dir.glob("src/asm/#{arch}/*.s").each do |file|
+  obj = "build/#{File.basename(file, '.s')}.o"
+  temp_file = "build/#{File.basename(file, '.s')}_temp.s"
 
-c_srcs = Dir.glob("src/*.c").join(" ")
-out_files = Dir.glob("build/*.o").join(" ")
-
-cbuild_args_str = cbuild_args.join(" ")
-run("gcc  #{cbuild_args_str} #{c_srcs} #{out_files}")
-if File.exist?("a.out")
-  FileUtils.mv("a.out", "build/main")
-end
-
-if install
-  run("ar rcs build/libkcstd.a #{out_files}")
-  install_kstd()
-else
-  if run
-    out_dir = ENV["HOME"] + "/temp/kltstd"
-
-    FileUtils.mkdir_p(out_dir)
-    FileUtils.cp("build/main", "#{out_dir}/main");
-    run("chmod +x #{out_dir}/main")
-    run("#{out_dir}/main")
+  # Skip file if last build file content is equal current
+  it_content = File.read(file)
+  if File.exist?(temp_file)
+    temp_file_content = File.read(temp_file)
+    if temp_file_content == it_content
+      next
+    else
+      File.write(temp_file, it_content)
+    end
+  else
+    File.write(temp_file, it_content)
   end
+
+  # Compile
+  puts "-- Compiling #{file}..."
+  run("gcc #{asmbuild_args} -c #{file} -o #{obj}")
+end
+
+# Compile All C Sources
+Dir.glob("src/*.c").each do |file|
+  obj = "build/#{File.basename(file, '.c')}.o"
+  temp_file = "build/#{File.basename(file, '.c')}_temp.c"
+
+  # Skip file if last build file content is equal current
+  it_content = File.read(file)
+  if File.exist?(temp_file)
+    temp_file_content = File.read(temp_file)
+    if temp_file_content == it_content
+      next
+    else
+      File.write(temp_file, it_content)
+    end
+  else
+    File.write(temp_file, it_content)
+  end
+
+  # Compile
+  puts "-- Compiling #{file}..."
+  run("gcc #{cbuild_args} -c #{file} -o #{obj}")
+end
+
+puts "-- Compiled successfully!"
+
+if install_arg
+  objects = Dir.glob("build/*.o").join(" ")
+  run("ar rcs build/libkcstd.a #{objects}")
+  install()
 end
